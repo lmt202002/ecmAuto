0264e41818db7c0fc7f7df69f40be76624467d6e
吕水荣 <lvshr@ecmaster.cn>
2017-08-22 11:52:55 +0800

    teset

--- src/main/test/case/bgCatchStock.java
+++ src/main/test/case/bgCatchStock.java
@@ -4,9 +4,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
 public class bgCatchStock {
-    public static void catchStock(WebDriver driver , String domain,String goodsName) throws InterruptedException {
+    public static int catchStock(WebDriver driver , String domain,String goodsName) throws InterruptedException {
         /**
-        访问总部库存管理页面,并搜索商品goodsName，获取库存；
+        访问总部库存管理页面,并搜索商品goodsName，获取库存，返回值为库存数值；
          */
         driver.get(domain+"/admin/agentProduct/index");//访问总部库存管理页面
 //        driver.navigate().refresh();
@@ -16,6 +16,11 @@ public class bgCatchStock {
 //        action.click(searchDiv).perform();
         driver.findElement(By.id("productName")).sendKeys(goodsName); //输入商品名称
         driver.findElement(By.xpath(".//*[@id='inventoryPanel']/div[1]/div[1]/div[2]/div[1]/div[1]/button")).click();//点击查询
-        //待补充
+        Thread.sleep(2000);
+        String stringStock=driver.findElement(By.xpath(".//*[@id='grid']/div/div[3]/table/tbody/tr[2]/td[8]/div/span")).getText();
+        int stock=Integer.parseInt(stringStock);
+//        System.out.println(stock);
+        return stock;
+
     }
 }
--- src/main/test/excutecase.java
+++ src/main/test/excutecase.java
@@ -6,17 +6,30 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 //import org.openqa.selenium.ie.InternetExplorerDriver;
 import common.*;
 
+import java.util.List;
+
 public class excutecase {
     public static void main(String[] args ) throws InterruptedException {
         WebDriver driver=new ChromeDriver();
+//        driver.manage().timeouts().implicitlyWait(10, 3);
         driver.manage().window().maximize();
         String domain="http://o2oagent.ecmaster.cn";
-        Session.bgLogin(driver,domain);
+        String user="13055203098";
+        Session.login(driver,domain,user);
+//        Session.bgLogin(driver,domain);
 //        bgAuditOrder.auditOrder(driver,domain,true);//后台总部审核操作
 //        String bigCode=bgAddSecurityCode.addCode(driver,domain);//后台添加大码操作
 //        bgBindCode.bindCode(driver,domain,bigCode);//后台绑定大码操作
 //        bgSendGoods.sendGoods(driver,domain,bigCode);//后台发货操作
-        bgCatchStock.catchStock(driver,domain,"新商品06");
+//        bgCatchStock.catchStock(driver,domain,"新商品06");//获取指定商品库存
+//采购下单及获取订单信息
+//        List <String> List1 =fgGetBill.getBill(driver,domain);
+//        float money=Float.parseFloat(List1.get(0));
+//        int goodsnum=Integer.parseInt(List1.get(1));
+//        String billnum=List1.get(2);
+//        System.out.println("订单总额："+money+"\n订单第一款商品数量："+goodsnum+"\n订单号："+billnum);
+
+
 
 
 

0df66c62878eafd629f692ecb89a0d5108b8a212
吕水荣 <lvshr@ecmaster.cn>
2017-09-01 08:58:45 +0800

    test

--- src/main/test/case/common/FgStep.java
+++ src/main/test/case/common/FgStep.java
@@ -16,10 +16,17 @@ public class FgStep {
          前台代理商审核下级订单，并按提供的参数点同意或拒绝，并返回订单金额和订单商品数量
          */
         driver.get(domain+"/m/agent/admin/order/subOrderUnCheckList?limit=10&start=0");//访问待审核下级订单列表页面
-        Thread.sleep(2000);
+//        try {
+//            int recodeNumber=driver.findElements(By.xpath(".//*[@id='page']/div[2]/div/div")).size();
+//        }catch (Exception e){
+//
+//        }
+//
+
+//        Thread.sleep(2000);
         List<WebElement> links=driver.findElements(By.linkText("审核"));//获取所有审核链接
         links.get(0).click();//点击第一个的审核
-        Thread.sleep(2000);
+//        Thread.sleep(2000);
         float money=0;
         float number=0;
         if (status) {

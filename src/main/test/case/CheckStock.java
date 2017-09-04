186f68c765d90d82f1e49929524cf8cd74a00274
吕水荣 <lvshr@ecmaster.cn>
2017-09-04 18:05:07 +0800

    test

--- src/main/test/case/CheckStock.java
+++ src/main/test/case/CheckStock.java
@@ -3,6 +3,8 @@ import common.RWuserData;
 import common.Session;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 import java.io.IOException;
 import java.util.List;
@@ -11,9 +13,15 @@ import java.util.concurrent.TimeUnit;
 
 public class CheckStock {
     public static void main(String[] args ) throws InterruptedException, IOException {
+
+
+    }
+    @Test
+    public void testCheckStock() throws IOException, InterruptedException {
         /**前台微商检查指定商品库存
          * 先判断CheckStock01.json文件是否存在，如果不存在，则继续
          * 并把返回的商品名称、已收库存、待收库存、订单号写入CheckStock01.json文件，设置状态值为：0，第一次检查时创建，1，第二次检查；
+         * 操作顺序为，读取库存，审核通过，再读取库存，然后比较两次库存是不是相差了审核的数量
          */
         WebDriver driver=new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
@@ -21,12 +29,12 @@ public class CheckStock {
         String wechat="atyj4";
         String userFilePath="E:\\github\\company\\src\\main\\test\\case\\common\\userData.json";
         String stockPath="E:\\github\\company\\src\\main\\test\\case\\testdata\\CheckStock01.json";
+        String goodSnumPath="E:\\github\\company\\src\\main\\test\\case\\testdata\\TestFgAuditOrder01.json";
         String goodsName="新商品0621";
         String status="0";
         Map map= RWuserData.readUserData(userFilePath,wechat).toMap();//取出指定用户的数据为Map
         String userPhone=map.get("phone").toString();//从map中取出用户手机号
         Session.login(driver,domain,userPhone);//前台登录
-//        List<String> userList=new ArrayList<String>();
         List<String> list1;
         try {
             if (RWuserData.readTestData(stockPath,"项目4").equals("0"))       //CheckStock01.json文件存在没异常则判断读取次数，如果是0，即创建后没有再读取，则修改状态为1；
@@ -41,11 +49,14 @@ public class CheckStock {
             list1.add(wechat);//添加库存所有人标记
             list1.add(status);
         }
-
-
+//        System.out.println(list1.get(0)+"\n"+list1.get(1));
         RWuserData.writeTestJSON(list1,"CheckStock01");//写结果数据到CheckStock01.json
+        int stockOld=Integer.parseInt(list1.get(1));//当前待收库存
+        FgStep.auditOrder(driver,domain,true);
+        int stockNow=stockOld+Integer.parseInt(RWuserData.readTestData(goodSnumPath,"项目1"));//审核的商品数量
 
         Session.logout(driver,domain);//前台退出
         driver.close();
+        Assert.assertEquals(stockNow,stockOld);//比较测试
     }
 }
